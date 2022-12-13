930aedbbb7193e864dede345e82243803f96ccfa
FeeinCof <tuan.letrong.dev@gmail.com>
2022-12-14 01:23:52 +0700

    style css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
+import HomePage from './pages/Home/Home';
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={< HomePage />} exact/>
+        <Route path='*' element={<Navigate to='/' replace />}/>
+      </Routes>
     </div>
   );
 }
